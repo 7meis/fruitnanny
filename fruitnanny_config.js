2ef1304406198ff24f1181b650a6842d8672bad8
Simon Meister <simon.meister@wagner.ch>
2025-01-01 23:58:54 +0100

    add irth date

--- fruitnanny_config.js
+++ fruitnanny_config.js
@@ -1,6 +1,6 @@
 let config = {
-    "baby_name": "Matthew",
-    "baby_birthday": "2016-03-15",
+    "baby_name": "Strampli",
+    "baby_birthday": "2024-09-18",
     "temp_unit": "C"
 }
 module.exports = config;
